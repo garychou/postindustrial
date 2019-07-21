60d360d3488a6edcb287822c111e46fc734535d7
garychou <gary@manja.org>
2019-07-21 15:46:50 -0400

    hmm

---
+++ .vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'PIDS',
+  description: 'Just playing around',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+    ]
+  },
+  sidebar: 'auto'
+}
